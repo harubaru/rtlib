905e74f281b2bd681b90395ceb3e85869c5cb6c1
Anthony Mercurio <amercurio@coreweave.com>
2024-06-18 15:04:14 -0700

    style(prototype): reformat file

--- prototype/rt.py
+++ prototype/rt.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 # Surface & PPM Code
 
 class Surface:
@@ -7,21 +8,21 @@ class Surface:
         self.w = w
         self.h = h
         self.buffer = [[0.0, 0.0, 0.0]] * (w * h)
-    
+
     def blit(self, pixels):
         for y in range(self.h):
             for x in range(self.w):
                 idx = (y * self.w) + x
                 self.buffer[idx] = pixels[idx]
-    
+
     def encode_pixel(self, x: int, y: int, r: float, g: float, b: float):
         self.buffer[(y * self.w) + x] = [r, g, b]
 
     def decode_pixel(self, x: int, y: int) -> list:
         return self.buffer[(y * self.w) + x]
-    
+
     def aspect_ratio(self) -> float:
-        return self.w/self.h
+        return self.w / self.h
 
     def generate_checkerboard(self, square_size):
         for y in range(self.h):
@@ -30,6 +31,7 @@ class Surface:
                     self.encode_pixel(x, y, 0.0, 0.0, 0.0)  # Black
                 else:
                     self.encode_pixel(x, y, 1.0, 1.0, 1.0)  # White
+
 
 def write_surface(filepath: str, surface: Surface) -> None:
     with open(filepath, "w") as f:
@@ -45,6 +47,7 @@ def write_surface(filepath: str, surface: Surface) -> None:
 
                 f.write(f"{ir} {ig} {ib}\n")
 
+
 # RT Primitives
 
 class Ray:
@@ -55,39 +58,35 @@ class Ray:
     ):
         self.origin = origin
         self.direction = direction
-    
+
     def at(self, t: float):
         return self.origin + t * self.direction
+
 
 class Camera:
     def __init__(
             self,
             surface,
-            samples_per_pixel = 10,
-            max_depth = 10,
-            vfov = 90.0,
-            look_from = np.array([0.0, 0.0, 0.0]),
-            look_at = np.array([0.0, 0.0, -1]),
-            up = np.array([0.0, 1.0, 0.0])
+            vfov=90.0,
+            look_from=np.array([0.0, 0.0, 0.0]),
+            look_at=np.array([0.0, 0.0, -1]),
+            up=np.array([0.0, 1.0, 0.0])
     ):
         self.surface = surface
-        self.samples_per_pixel = samples_per_pixel
-        self.pixel_sample_scale = 1 / self.samples_per_pixel
-        self.max_depth = max_depth
         self.vfov = vfov
         self.look_from = look_from
         self.look_at = look_at
         self.up = up
 
         theta = vfov * np.pi / 180.0
-        h = np.tan(theta/2)
+        h = np.tan(theta / 2)
         viewport_height = 2 * h
         viewport_width = viewport_height * self.surface.aspect_ratio()
 
         w = self.look_from - self.look_at
-        w_hat = w / (w**2).sum()**0.5
+        w_hat = w / (w ** 2).sum() ** 0.5
         u = np.cross(self.up, w)
-        u_hat = u / (u**2).sum()**0.5
+        u_hat = u / (u ** 2).sum() ** 0.5
         v = np.cross(w_hat, u_hat)
 
         viewport_u = viewport_width * u
@@ -96,9 +95,9 @@ class Camera:
         self.pixel_delta_u = viewport_u / self.surface.w
         self.pixel_delta_v = viewport_v / self.surface.h
 
-        viewport_upper_left = self.look_from - (w) - viewport_u/2 - viewport_v/2
+        viewport_upper_left = self.look_from - (w) - viewport_u / 2 - viewport_v / 2
         self.pixel00_loc = viewport_upper_left + 0.5 * (self.pixel_delta_u + self.pixel_delta_v)
-    
+
     def ray(self, x, y):
         pixel_sample = self.pixel00_loc + (x * self.pixel_delta_u) + (y * self.pixel_delta_v)
 
@@ -107,34 +106,33 @@ class Camera:
 
         return Ray(ray_origin, ray_direction)
 
+
 class Sphere:
     def __init__(self, center, radius):
         self.center = np.array(center)
         self.radius = radius
-    
+
     def intersect(self, ray):
         oc = ray.origin - self.center
         a = np.dot(ray.direction, ray.direction)
         b = 2.0 * np.dot(oc, ray.direction)
-        c = np.dot(oc, oc) - self.radius**2
-        discriminant = b**2 - 4*a*c
+        c = np.dot(oc, oc) - self.radius ** 2
+        discriminant = b ** 2 - 4 * a * c
         if discriminant > 0:
-            t1 = (-b - np.sqrt(discriminant)) / (2.0*a)
-            t2 = (-b + np.sqrt(discriminant)) / (2.0*a)
+            t1 = (-b - np.sqrt(discriminant)) / (2.0 * a)
+            t2 = (-b + np.sqrt(discriminant)) / (2.0 * a)
             if t1 < t2 and t1 > 0:
                 return t1
             if t2 > 0:
                 return t2
         return None
 
-    def bounding_box(self):
-        return AABB(self.center - self.radius, self.center + self.radius)
 
 class Plane:
     def __init__(self, point, normal):
         self.point = np.array(point)
         self.normal = np.array(normal)
-    
+
     def intersect(self, ray):
         denom = np.dot(self.normal, ray.direction)
         if np.abs(denom) > 1e-6:
@@ -143,9 +141,6 @@ class Plane:
                 return t
         return None
 
-    def bounding_box(self):
-        inf = float('inf')
-        return AABB(np.array([-inf, -inf, -inf]), np.array([inf, inf, inf]))
 
 # Acceleration Structures
 
@@ -162,6 +157,7 @@ class NaiveAccelerationStructure:
                 closest_hit = t
                 closest_t = t
         return closest_hit
+
 
 class RayTracingPipeline:
     def __init__(self, accel_structure, ray_gen, intersect, any_hit, closest_hit, miss):
@@ -189,27 +185,34 @@ class RayTracingPipeline:
                     color = self.miss(ray)
                 surface.encode_pixel(x, y, *color)
 
+
 # testing stuff!!
 surface = Surface(400, 200)
 camera = Camera(surface)
 
+
 def ray_gen_function(x, y):
     return camera.ray(x, y)
+
 
 def intersect_function(ray, obj):
     return obj.intersect(ray)
 
+
 def any_hit_function(ray, hit):
     return True
+
 
 def closest_hit_function(ray, hit):
     return [1.0, 0.0, 0.0]  # red color for hits
 
+
 def miss_function(ray):
     return [0.0, 1.0, 0.0]  # black color for misses
 
+
 pipeline = RayTracingPipeline(
-    BVHNode([
+    NaiveAccelerationStructure([
         Sphere([0, 0, -5], 1),
         Sphere([2, 0, -5], 1),
         Plane([0, -1, 0], [0, 1, 0])
@@ -221,5 +224,5 @@ pipeline = RayTracingPipeline(
     miss_function
 )
 
-pipeline.trace_rays(surface, 4, 1)
+pipeline.trace_rays(surface, 1, 0)
 write_surface("output.ppm", surface)
